3df1d4a72fcbbddddc53f1517ecd068966ab5ef8
Sam Varner <snick-a-doo@comcast.net>
2021-10-11 20:25:56 -0400

    Corrected name of app in license comments.

--- lib4color/point.hh
+++ lib4color/point.hh
@@ -6,11 +6,11 @@
 // the GNU General Public License as published by the Free Software Foundation, either
 // version 3 of the License, or (at your option) any later version.
 //
-// Composure is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// 4color is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 // PURPOSE.  See the GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along with Composure.
+// You should have received a copy of the GNU General Public License along with 4color.
 // If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef FOUR_COLOR_LIB4COLOR_POINT_HH_INCLUDED
